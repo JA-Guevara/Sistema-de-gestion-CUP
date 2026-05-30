c48a7d4964501e515c31f8039ce29fa5db7f7f91
Jose A. GUevara <jose.guevara1caballero@gmail.com>
2026-05-30 09:21:05 -0400

    Fix Vercel PHP entrypoint

--- api/index.php
+++ api/index.php
@@ -2,4 +2,19 @@
 
 declare(strict_types=1);
 
-return require dirname(__DIR__).'/public/index.php';
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+if (file_exists(dirname(__DIR__).'/.env')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'prod', (bool) ($_SERVER['APP_DEBUG'] ?? false));
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+
+$response->send();
+$kernel->terminate($request, $response);
