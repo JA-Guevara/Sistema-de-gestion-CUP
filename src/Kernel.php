28249397ad355fd77ffe29863e31b5b672a40816
Jose A. GUevara <jose.guevara1caballero@gmail.com>
2026-05-30 09:47:30 -0400

    Use tmp cache on Vercel

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,27 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        if ($this->isRunningOnVercel()) {
+            return sys_get_temp_dir().'/symfony-cache/'.$this->environment;
+        }
+
+        return parent::getCacheDir();
+    }
+
+    public function getLogDir(): string
+    {
+        if ($this->isRunningOnVercel()) {
+            return sys_get_temp_dir().'/symfony-logs';
+        }
+
+        return parent::getLogDir();
+    }
+
+    private function isRunningOnVercel(): bool
+    {
+        return (bool) ($_SERVER['VERCEL'] ?? $_ENV['VERCEL'] ?? getenv('VERCEL'));
+    }
 }
